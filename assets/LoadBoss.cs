70c09a3fed20245af8591223c96a23e01ff5b766
xlucy <xlucyyu@gmail.com>
2025-05-01 17:57:01 +0200

    added bosses to challenge editor

--- assets/LoadBoss.cs
+++ assets/LoadBoss.cs
@@ -11,25 +11,24 @@ namespace CTTileToChallenge.assets;
 [HarmonyPatch(typeof(ChallengeEditorPlay), nameof(ChallengeEditorPlay.StartNewGame))]
 public static class LoadBoss 
 {
-    private static BossType ConvertIntToBossType(int bossID)
-    {
-        return bossID switch
+   private static BossType ConvertIntToBossType(int bossID)
+   {
+        switch (bossID)
         {
-            0 => BossType.Bloonarius,
-            1 => BossType.Lych,
-            2 => BossType.Vortex,
-            3 => BossType.Dreadbloon,
-            4 => BossType.Phayze,
-            5 => BossType.Blastapopoulos,
-            _ => BossType.Bloonarius
-        };
-    }
+            case 0: return BossType.Bloonarius;
+            case 1: return BossType.Lych;
+            case 2: return BossType.Vortex;
+            case 3: return BossType.Dreadbloon;
+            case 4: return BossType.Phayze;
+            case 5: return BossType.Blastapopoulos;
+            default: return BossType.Bloonarius; 
+        }
+   }
 
     [HarmonyPrefix]
     private static void Prefix()
     {
-        var bossType = Main.selectedTileDataJson?.GameData?.bossData?.bossBloon;
-
+        var bossType = Main.selectedTileDataJson?.GameData?.bossData?.bossBloon; // bosses are stored as integers in the api
         if (bossType == null) return;
 
         BossType bossElement = ConvertIntToBossType(bossType.Value);
